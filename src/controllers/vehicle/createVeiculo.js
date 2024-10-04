e79eb75f1e9c85e51c28e89fc77f5e7b462797b9
GustavoRodriguesk <gustavorodrig780@gmail.com>
2024-10-04 16:36:34 -0300

    sem bala contada

--- src/controllers/vehicle/createVeiculo.js
+++ src/controllers/vehicle/createVeiculo.js
@@ -3,24 +3,16 @@
 import { Router } from 'express';
 import { createVeiculo } from '../../models/vehicleModel.js'; 
 import multer from 'multer';
-import path from 'path';
 
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, path.resolve("uploads")); 
-    },
-    filename: (req, file, callback) => {
-        const time = Date.now();
-        callback(null, `${time}_${file.originalname}`);
-    }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
 
 const router = Router();
 
 router.post('/', upload.single('foto'), async (req, res) => {
     const { modelo, anoFabricacao, cor, descricao, valor, km, marca, usuarioId, cidade, estado, cep, complemento, logradouro, numero, cambio, carroceria, combustivel } = req.body;
+    const fotoBuffer = req.file ? req.file.buffer : null;
 
     if (!req.file) {
         return res.status(400).json({ message: 'A foto do veículo é obrigatória.' });
@@ -35,7 +27,7 @@ router.post('/', upload.single('foto'), async (req, res) => {
             valor: parseFloat(valor),
             km: parseFloat(km),
             marca,
-            foto: req.file.filename, 
+            foto: fotoBuffer,  // Armazena a imagem como Bytea
             usuarioId: parseInt(usuarioId),
             cidade,
             estado,
