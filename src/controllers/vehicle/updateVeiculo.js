932b55033ff36dcd6ec1173e6962ee80d9a6e026
GustavoRodriguesk <gustavorodrig780@gmail.com>
2024-10-04 17:43:42 -0300

    andreas

--- src/controllers/vehicle/updateVeiculo.js
+++ src/controllers/vehicle/updateVeiculo.js
@@ -9,10 +9,35 @@ const router = Router();
 
 router.put('/:id', upload.single('foto'), async (req, res) => {
     const { id } = req.params;
-    const { modelo, anoFabricacao, cor, descricao, valor, km, marca, usuarioId, cidade, estado, cep, complemento, logradouro, numero, cambio, carroceria, combustivel } = req.body;
+    const {
+        modelo,
+        anoFabricacao,
+        cor,
+        descricao,
+        valor,
+        km,
+        marca,
+        usuarioId,
+        cidade,
+        estado,
+        cep,
+        complemento,
+        logradouro,
+        numero,
+        cambio,
+        carroceria,
+        combustivel
+    } = req.body;
+
+    if (isNaN(id)) {
+        return res.status(400).json({ message: 'ID deve ser um número válido.' });
+    }
+
     const fotoBuffer = req.file ? req.file.buffer : null;
 
-    if (!modelo || !anoFabricacao || !cor || !descricao || !valor || !km || !marca || !usuarioId) {
+    // Check for required fields
+    const requiredFields = [modelo, anoFabricacao, cor, descricao, valor, km, marca, usuarioId];
+    if (requiredFields.some(field => !field)) {
         return res.status(400).json({ message: 'Todos os campos obrigatórios devem ser preenchidos.' });
     }
 
@@ -49,10 +74,9 @@ router.put('/:id', upload.single('foto'), async (req, res) => {
         const updatedVeiculo = await updateVeiculo(id, veiculoData);
         res.status(200).json(updatedVeiculo);
     } catch (error) {
-        console.error(error);
+        console.error('Error updating vehicle:', error);
         res.status(500).json({ message: 'Erro ao atualizar veículo.' });
     }
 });
-
 
 export default router;
