c0f6f2cfaa4bd9b79e5f532559ca1ab13ff00a76
Gustavo <rodrigues.gustavo2@aluno.ifsp.edu.br>
2024-10-16 14:49:46 -0300

    ultimo

--- src/controllers/vehicle/getVeiculo.js
+++ src/controllers/vehicle/getVeiculo.js
@@ -1,20 +1,15 @@
 import { getveiculo } from "../../models/vehicleModel.js" 
 
 const list = async (req, res, next) => {
-    try {
-        const veiculos = await getveiculo();
-        const veiculosComImagem = veiculos.map(veiculo => ({
-            ...veiculo,
-            foto: veiculo.foto ? `data:image/jpeg;base64,${veiculo.foto.toString('base64')}` : null
-        }));
-
+    try{
+        const veiculos = await getveiculo()
         return res.json({
-            message: "Veículos carregados com sucesso!",
-            veiculos: veiculosComImagem
-        });
-    } catch (error) {
-        next(error);
+            message: "veiculos com sucesso!",
+            veiculos
+        })
+    } catch(error) {
+        next(error)
     }
-};
+}
 
 export default list
